536c3bed2d0ac3e89ab7874ca76f0014c562a612
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-14 15:53:01 -0400

    Initial commit

---
+++ bspwm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+xbps-install -Syu &&  # i'll be recommending plain install from web not image buuut
+xbps-install gcc make pkg-config git atop htop vim -y &&  # install build tools and a few utilities
+xbps-install bspwm feh font-awesome5 maim picom polybar rofi scrot sxhkd xclip -y &&  # install bspwm thing
+# copy mandatory config files
+# in theory if doas with preserve env is set, ~ will be towards the user directory
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc &&
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc &&
+sed -i 's/urxvt/st' ~/.config/sxhkdrc &&
+# echo "\nprint_screen\n\tscrot"  >> ~/.config/sxhkdrc
+# attempt to put print screen keybind
+# my favorite part :)
+xbps-install libXinerama-devel libXft-devel libXext-devel ncurses-devel -y &&
+mkdir /root/.config/suckless &&  # hopefully doesnt error due to .config not existing beforehand
+cd /root/.config/suckless &&
+git clone https://git.suckless.org/st &&
+cd st &&
+make clean install &&  # will very much error due to dependencies i also forgor :skull:
+cd .. &&
+git clone https://git.suckless.org/dmenu &&
+make clean install &&
+
+echo "exec bspwm" > ~/.xinitrc
---
+++ lightdm.sh
@@ -0,0 +1,12 @@
+#/bin.bash
+xbps-install make pkg-config git vim &&
+xbps-install lightdm lightdm-webkit2-greeter dbus-elogind &&
+cd /usr/share/lightdm-webkit/themes &&
+git clone https://github.com/ffwff &&
+# set webkit2 to use modern
+# set lightdm to invoke webkit2
+ln -s /etc/sv/lightdm /var/service &&
+ln -s /etc/sv/acpid /var/service &&
+ln -s /etc/sv/dbus /var/service &&
+ln -s /etc/sv/polkitd /var/service &&
+ln -s /etc/sv/uuidd /var/service
