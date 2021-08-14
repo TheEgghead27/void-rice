b406ab737ac48686c7732ed4ebe2ee0f168d5115
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-14 16:38:37 -0400

    add automagic alias

--- doas.sh
+++ doas.sh
@@ -3,3 +3,4 @@ xbps-install opendoas -y &&
 echo "permit persist keepenv :wheel as root" > /etc/doas.conf &&
 echo "ignorepkg=sudo" > /etc/xbps.d/sudo.conf &&
 xbps-remove sudo -y
+echo alias sudo='doas'>> ~/.bashrc 
