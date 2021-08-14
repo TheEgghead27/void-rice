c0fcb73543515088efee26ac3a14c186c5d37aca
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-14 16:51:31 -0400

    fix

--- lightdm.sh
+++ lightdm.sh
@@ -3,9 +3,11 @@
 # xbps-install make pkg-config git vim &&
 xbps-install lightdm lightdm-webkit2-greeter dbus-elogind -y &&
 cd /usr/share/lightdm-webkit/themes &&
-git clone https://github.com/ffwff &&
+git clone https://github.com/ffwff/modern &&
 # set webkit2 to use modern
+sed -i 's/= void/= modern'
 # set lightdm to invoke webkit2
+sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter' /etc/lightdm/lightdm.conf
 ln -s /etc/sv/lightdm /var/service &&
 ln -s /etc/sv/acpid /var/service &&
 ln -s /etc/sv/dbus /var/service &&
