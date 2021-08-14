efe7f06d4ff0e5732142da3f597e6b7d7d858f32
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-14 16:33:53 -0400

    add curl

--- essentials.sh
+++ essentials.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xbps-install -Syu &&
 xbps-install gcc make pkg-config git &&  # build essentialss
-xbps-install vim nano htop atop &&  # utilities
+xbps-install vim nano htop curl atop &&  # utilities
