87dde83d47b7fb5ffec483841c480677f119c94c
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-14 16:37:43 -0400

    epic doas fail

--- doas.sh
+++ doas.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-xbps-install doas -y &&
+xbps-install opendoas -y &&
 echo "permit persist keepenv :wheel as root" > /etc/doas.conf &&
 echo "ignorepkg=sudo" > /etc/xbps.d/sudo.conf &&
 xbps-remove sudo -y
