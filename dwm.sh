339907304e97103fdfc3f6e0d4890d763975b06f
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-18 21:29:46 -0400

    Bug fixes

--- dwm.sh
+++ dwm.sh
@@ -1,12 +1,12 @@
 xbps-install feh pulseaudio redshift scrot xclip xcompmgr -y &&
-xbps-install libX11-devel libXft-devel libXinerama-devel libXft-devel libXext-devel ncurses-devel -y &&
+xbps-install libX11-devel libXft-devel libXinerama-devel libXft-devel libXrandr-devel libXext-devel ncurses-devel -y &&
 # no idea if these are needed
 # xbps-install libXScrnSaver-devel libXau-devel libXcomposite-devel libXcursor-devel libXdamage-devel libXdmcp-devel libXfixes-devel libXi-devel libXrandr-devel libXrender-devel libXtst-devel libXxf86vm-devel libxcb-devel libxkbcommon-devel libxml2-devel libxshmfence-devel &&
 git submodule update --init --recursive &&  # ensure submodules
 cd suckless/dwm &&
 make clean install &&
 cp ../.dwm/powermenu.sh /usr/local/bin/powermenu &&
-cp ../.dwm /home/$(logname)/ &&
+cp -r ../.dwm /home/$(logname)/ &&
 cd ../st &&
 make clean install &&
 cd ../dmenu &&
